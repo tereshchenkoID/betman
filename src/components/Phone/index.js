0dc2da7f8c65e5e817a3aaaff76585dd36f09fe2
tereshchenkoID <teren23041991@ukr.net>
2026-08-24 16:57:35 +0300

    - add react-international-phone

--- src/components/Phone/index.js
+++ src/components/Phone/index.js
@@ -1,24 +1,22 @@
 'use client'
 
 import { useLocale } from 'next-intl'
-import { useEffect, useMemo, useRef, useState, useTransition } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState, useTransition } from 'react'
+import { usePhoneInput, CountrySelector, defaultCountries } from 'react-international-phone'
 import clsx from 'clsx'
-import PhoneInput from 'react-phone-input-2'
 
 import { runRules } from '@/helpers/rules'
 import { action } from './action'
 
 import Preload from '@/components/Preload'
 
-import 'react-phone-input-2/lib/style.css'
-import './index.scss'
+import 'react-international-phone/style.css'
 import style from './index.module.scss'
 
 const countriesCache = {}
 
 const Phone = ({
   data,
-  country,
   onChange,
   placeholder,
   onValidate,
@@ -34,7 +32,7 @@ const Phone = ({
 
   const [countries, setCountries] = useState(countriesCache[locale] || null)
   const [isPending, startTransition] = useTransition()
-  const [isMounted, setIsMounted] = useState(true)
+  const [isLoading, setIsLoading] = useState(!countriesCache[locale])
 
   const inputRef = useRef(null)
 
@@ -56,56 +54,90 @@ const Phone = ({
   }
 
   useEffect(() => {
+    let isSubscribed = true
+
     if (countriesCache[locale]) {
       startTransition(() => {
         setCountries(countriesCache[locale])
-        setIsMounted(false)
+        setIsLoading(false)
       })
+
       return
     }
 
     startTransition(async () => {
-      const res = await action()
-      if (res) {
-        countriesCache[locale] = res
-        setCountries(res)
-        setIsMounted(false)
+      setIsLoading(true)
+
+      try {
+        const res = await action()
+        if (isSubscribed && res) {
+          countriesCache[locale] = res
+          setCountries(res)
+        }
+      } finally {
+        if (isSubscribed) {
+          setIsLoading(false)
+        }
       }
     })
 
     return () => {
-      setIsMounted(false)
+      isSubscribed = false
     }
   }, [locale])
 
-  const localization = useMemo(() => {
-    if (!countries) return undefined
+  const localizedCountries = useMemo(() => {
+    if (!countries) return defaultCountries
 
-    if (Array.isArray(countries)) {
-      return countries.reduce((acc, item) => {
-        const code = item.code || item.iso2
-        const name = item.text || item.name || item.title
-        if (code && name) {
-          acc[code.toLowerCase()] = name
-        }
-        return acc
-      }, {})
-    }
+    const rawList = Array.isArray(countries) ? countries : countries?.data
+    if (!rawList) return defaultCountries
 
-    return countries?.data.reduce((acc, item) => {
-      acc[item.alpha_2.toLowerCase()] = item.label
+    const namesMap = rawList.reduce((acc, item) => {
+      const code = (item.code || item.iso2 || item.alpha_2)?.toLowerCase()
+      const name = item.text || item.name || item.title || item.label
+      if (code && name) acc[code] = name
       return acc
     }, {})
+
+    return defaultCountries.map((c) => {
+      const iso2 = c[1]
+      const customName = namesMap[iso2]
+
+      if (customName) {
+        const updated = [...c]
+        updated[0] = customName
+        return updated
+      }
+
+      return c
+    })
   }, [countries])
 
-  if (isMounted) {
+  const {
+    phone,
+    handlePhoneValueChange,
+    inputRef: phoneInputRef,
+    country: currentCountry,
+    setCountry,
+  } = usePhoneInput({
+    defaultCountry: 'us',
+    value: data || '',
+    countries: localizedCountries,
+    onChange: (phoneData) => onChange?.(phoneData.phone),
+  })
+
+  const setCombinedRef = useCallback((node) => {
+    phoneInputRef.current = node
+    inputRef.current = node
+  }, [phoneInputRef])
+
+  if (isLoading) {
     return <Preload count={1} className={style.skeleton} />
   }
 
   if (countries?.meta?.results === '0') return null
 
-  const isFilled = data && data.length > 3
-  const isLabelActive = focused || isFilled
+  const isFilled = phone && phone.length > 3
   const showError = error && (touched || (isFilled && error !== runRules('', rules)))
 
   return (
@@ -114,50 +146,51 @@ const Phone = ({
         clsx(
           style.block,
           {
-            [style.disabled]: isDisabled,
-            [style.focused]: isLabelActive,
+            [style.disabled]: isDisabled || isPending,
+            [style.focused]: focused,
             [style.error]: showError,
           }
         )
       }
     >
+      <label
+        className={style.label}
+        onClick={() => inputRef.current?.focus()}
+      >
+        {placeholder}
+        {isRequired && <span>*</span>}
+      </label>
       <div className={style.wrapper}>
-        <label
-          className={style.label}
-          onClick={() => inputRef.current?.focus()}
-        >
-          {placeholder}
-          {isRequired && <span>*</span>}
-        </label>
-
-        <PhoneInput
+        <CountrySelector
           key={locale}
-          ref={inputRef}
-          localization={localization}
-          disabled={isPending || isDisabled}
-          inputProps={{
-            name: 'phone',
-            required: isRequired,
-            onBlur: handleBlur,
-            onFocus: () => setFocused(true),
-          }}
-          containerClass={
-            clsx(
-              style['react-tel-input'],
-              {
-                [style.filled]: isFilled,
-              }
-            )
-          }
-          disableCountryCode={false}
-          placeholder={placeholder}
-          country={country}
+          selectedCountry={currentCountry.iso2}
+          onSelect={(countryData) => setCountry(countryData.iso2)}
+          disabled={isDisabled || isPending}
+          countries={localizedCountries}
           preferredCountries={['ua', 'es', 'gb', 'uz', 'ru']}
-          value={data}
-          onChange={(value) => onChange(value)}
+          buttonClassName={style.flag}
+          dropdownStyleProps={{
+            className: style.dropdown,
+          }}
+        />
+        <input
+          ref={setCombinedRef}
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={handlePhoneValueChange}
+          onFocus={() => setFocused(true)}
+          onBlur={handleBlur}
+          disabled={isDisabled || isPending}
+          required={isRequired}
+          placeholder={placeholder}
+          className={style.field}
         />
       </div>
-      {showError && <p className={style.message}>{error}</p>}
+      {
+        showError &&
+        <p className={style.message}>{error}</p>
+      }
     </div>
   )
 }
