133745790e8c44852548c44b1fd547edd55aa126
tereshchenkoID <teren23041991@ukr.net>
2026-09-01 18:48:46 +0300

    - add fr locale

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -1,7 +1,7 @@
 import { defineRouting } from 'next-intl/routing'
 
 export const routing = defineRouting({
-  locales: ['en', 'uk'],
+  locales: ['en', 'uk', 'fr'],
   defaultLocale: 'en',
   localePrefix: 'always',
 })
