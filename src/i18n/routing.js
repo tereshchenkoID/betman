5cb53d50e23e24af4c77bcb9fed3e687314f4c2c
tereshchenkoID <teren23041991@ukr.net>
2026-09-01 23:17:14 +0300

    - test viewport in telegram

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -1,7 +1,7 @@
 import { defineRouting } from 'next-intl/routing'
 
 export const routing = defineRouting({
-  locales: ['en', 'uk', 'fr'],
+  locales: ['en', 'fr'],
   defaultLocale: 'en',
   localePrefix: 'always',
 })
