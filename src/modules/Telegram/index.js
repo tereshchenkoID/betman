ee71e1449c50764513a014c29e9c74598508a5ea
tereshchenkoID <teren23041991@ukr.net>
2026-09-01 23:54:07 +0300

    - test viewport in telegram

--- src/modules/Telegram/index.js
+++ src/modules/Telegram/index.js
@@ -50,7 +50,7 @@ export default function Telegram({ auth }) {
         document.documentElement.style.setProperty('--tg-safe-bottom', `${bottom}px`)
         document.documentElement.style.setProperty('--tg-viewport-height', `${height}px`)
 
-        alert(bottom)
+        alert(top)
       }
 
       setTimeout(() => {
