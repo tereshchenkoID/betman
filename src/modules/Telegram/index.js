87cd352c9dd0f46982cf9b1876fba9d8d4ae8598
tereshchenkoID <teren23041991@ukr.net>
2026-09-13 22:14:12 +0300

    fix: "Fix toastify"

--- src/modules/Telegram/index.js
+++ src/modules/Telegram/index.js
@@ -52,6 +52,8 @@ export default function Telegram({ auth }) {
         document.documentElement.style.setProperty('--tg-safe-top', `${top}px`)
         document.documentElement.style.setProperty('--tg-safe-bottom', `${bottom}px`)
         document.documentElement.style.setProperty('--tg-viewport-height', `${height}px`)
+
+        document.documentElement.style.setProperty('--toastify-toast-top', `${top + 8}px`)
       }
 
       setTimeout(() => {
