c5e532528d16a5df3206954979db9758155501aa
tereshchenkoID <teren23041991@ukr.net>
2026-09-25 23:43:00 +0300

    feat: "Test payout"

--- src/modules/IframeBreaker/index.js
+++ src/modules/IframeBreaker/index.js
@@ -1,13 +1,28 @@
 'use client'
 
-import { useEffect } from 'react'
-
 export default function IframeBreaker() {
-  useEffect(() => {
-    if (window.self !== window.top) {
-      window.top.location.href = window.location.href
-    }
-  }, [])
+  const inlineScript = `
+    (function() {
+      try {
+        if (window.self !== window.top) {
+          var targetUrl = window.location.href;
+          // 1. Отправляем сообщение родителю
+          if (window.parent) {
+            window.parent.postMessage({ type: 'PAYMENT_SUCCESS_REDIRECT', url: targetUrl }, '*');
+          }
+          // 2. Мгновенно меняем URL верхнего окна
+          window.top.location.href = targetUrl;
+        }
+      } catch (e) {
+        // Фолбэк для Cross-Origin ограничений
+        try { window.open(window.location.href, '_top'); } catch (err) {}
+      }
+    })();
+  `
 
-  return null
+  return (
+    <script
+      dangerouslySetInnerHTML={{ __html: inlineScript }}
+    />
+  )
 }
